cd6c638a31b456c9809d5cb225752230ea5e3724
Graham Lyons <S00225961@atu.ie>
2023-05-15 16:54:35 +0100

    created class movie content and added DatabaseManagement to project

--- S00225961SummerExam/Booking.cs
+++ S00225961SummerExam/Booking.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text; 
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace S00225961SummerExam
 {
@@ -11,17 +12,21 @@ namespace S00225961SummerExam
         public int BookingID { get; set; }
         public DateTime BookingDate { get; set; }
         public int NumberOfTickets { get; set; }
+        public virtual Movie Movie { get; set; }
     }
     public class Movie
     {
         public int MovieID { get; set; }
         public string Title { get; set; }
+        public string ImageName { get; set; }
         public string Description { get; set; }
         public string Cast { get; set; }
     }
 
-    public class MovieData
+    public class MovieData : DbContext
     {
-
+        public MovieData() : base("MyMovieData") { }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
     }
 }
