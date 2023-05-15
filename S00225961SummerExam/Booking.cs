be403b09abbcbc4ed71a2fdfd25b95cd41d616a6
Graham Lyons <S00225961@atu.ie>
2023-05-15 16:45:10 +0100

    added entity frame work and create booking and move class

---
+++ S00225961SummerExam/Booking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S00225961SummerExam
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int NumberOfTickets { get; set; }
+    }
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Cast { get; set; }
+    }
+
+    public class MovieData
+    {
+
+    }
+}
